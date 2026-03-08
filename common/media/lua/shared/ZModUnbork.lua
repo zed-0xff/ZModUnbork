f2123575ef50e5f1aa63d9586f37d27c93c9ed30
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2026-03-09 01:45:53 +0200

    fix dirtiness for AlarmClockClothing

--- common/media/lua/shared/ZModUnbork.lua
+++ common/media/lua/shared/ZModUnbork.lua
@@ -36,6 +36,13 @@ end
 patch_method_alias(item, "getDirtiness", "getDirtyness")
 patch_method_alias(item, "setDirtiness", "setDirtyness")
 
+-- class AlarmClockClothing extends Clothing
+local clock = instanceItem("Base.WristWatch_Left_ClassicGold")
+if clock then
+    -- fixes ItemComparison mod errors when showing tooltip for watches
+    patch_method_alias(clock, "getDirtiness", "getDirtyness")
+    patch_method_alias(clock, "setDirtiness", "setDirtyness")
+end
 
 -- zombie/scripting/objects/Item.java
 --   42.12    public ArrayList<String> getTags()

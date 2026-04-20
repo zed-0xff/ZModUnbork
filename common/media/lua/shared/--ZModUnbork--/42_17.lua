0708ec6735c246736e23641338afaefd633a6f5f
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2026-04-20 21:49:32 +0300

    CombatText

--- common/media/lua/shared/--ZModUnbork--/42_17.lua
+++ common/media/lua/shared/--ZModUnbork--/42_17.lua
@@ -1,3 +1,6 @@
+-- mods unborked:
+--   CombatText.2286124931
+--   lots of mods using getVehicles()
 if getCore():getGameVersion():isLessThan(GameVersion.parse("42.17")) then return end
 
 -- zombie/iso/IsoMovingObject.java
@@ -16,3 +19,17 @@ zdk.augment_all_metatables("moveUnmodded", {
 --   42.16: public ArrayList<BaseVehicle> getVehicles()
 --   42.17: public Set<BaseVehicle> getVehicles()
 -- see tags.lua
+
+-- 42.16:
+--   IsoGameCharacter
+--     public boolean getIsNPC()
+--     public boolean isNPC()
+--     public void setIsNPC(boolean isAI)
+--     public void setNPC(boolean newvalue)
+-- 42.17:
+--   IsoGameCharacter
+--     public boolean isNpc()
+--   IsoPlayer
+--     public void setNpc(boolean isNpc)
+zdk.augment_all_metatables("isNpc",  { getIsNPC = "isNpc", isNPC = "isNpc" })
+zdk.augment_all_metatables("setNpc", { setNPC = "setNpc", setIsNPC = "setNpc" })
